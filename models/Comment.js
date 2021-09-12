e67998fe5ce258d063c6d96df9834358570b2bab
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-12 10:51:04 -0400

    fixed capitalization of models

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
 class Comment extends Model { }
 
 Comment.init(
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
+
 // create our User model
 class User extends Model {
     // set up method to run on instance data (per user) to check password
